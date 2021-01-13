f51f41f2f28e1fd41a9f0cbe2eb9a6e69d0819db
bryan_menendez <bryan.menendez@protonmail.com>
2021-01-13 20:35:00 -0300

    new models

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('indoors', 'Indoors'),
+        ('outdoors', 'Outdoors')
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('pending', 'Pending'),
+        ('delivering', 'Out on delivery'),
+        ('delivered', 'Delivered')
+    )
+
+    #customer =
+    #product = 
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
