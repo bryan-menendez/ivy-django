9502741bd79b6c67ed3629bb42ca8713360c4fa2
bryan_menendez <bryan.menendez@protonmail.com>
2021-01-16 01:04:08 -0300

    dynamic data rendering

--- accounting/views.py
+++ accounting/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
-    return render(request, 'accounting/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    
+    total_orders = orders.count()
+    pending_orders = orders.filter(status='pending').count()
+    delivered_orders = orders.filter(status='delivered').count()
+
+    context = {'orders':orders, 'customers':customers, 
+        'delivered_orders' : delivered_orders, 'total_orders' : total_orders,
+        'pending_orders' : pending_orders
+    }
+
+    return render(request, 'accounting/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounting/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounting/products.html', {'products' : products})
 
 def customers(request):
     return render(request, 'accounting/customers.html')
--- ivy/views.py
+++ ivy/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from accounting.models import Order
 
 def index(request):
-    return render(request, 'ivy/index.html')
+    total_orders = Order.objects.count()
+    pending_orders = Order.objects.filter(status='pending').count()
+    delivered_orders = Order.objects.filter(status='delivered').count()
+
+    context = {'delivered_orders' : delivered_orders, 'total_orders' : total_orders,
+        'pending_orders' : pending_orders
+    }
+
+    return render(request, 'ivy/index.html', context)
