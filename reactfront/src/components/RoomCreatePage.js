ad30520a5902384033e8b9325b44d03bb1f73b00
bryan_menendez <bryan.menendez@protonmail.com>
2021-01-25 03:14:46 -0300

    ajax creation of a room via react

--- reactfront/src/components/RoomCreatePage.js
+++ reactfront/src/components/RoomCreatePage.js
@@ -15,6 +15,64 @@ export default class RoomCreatePage extends Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            guestCanPause: false,
+            votesToSkip: this.default_votes,
+        };
+
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+        this.handleRoomCreatePressed = this.handleRoomCreatePressed.bind(this);
+    }
+
+    handleVotesChange(e){
+        this.setState({
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handleGuestCanPauseChange(e){
+        this.setState({
+            guestCanPause: e.target.value === "true" ? true : false,
+        });
+    }
+
+    handleRoomCreatePressed(e){
+        //console.log(this.state)
+        const csrftoken = this.getCookie('csrftoken');
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json',
+                'X-CSRFToken': csrftoken},
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause
+            })
+        };
+
+        fetch('/api/rooms/create', requestOptions).then( 
+            (response) => response.json()
+        ).then( 
+            (data) => console.log(data)
+        );
+    }
+    
+    getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
     }
 
     render(){ return( 
@@ -27,7 +85,7 @@ export default class RoomCreatePage extends Component{
                     <FormHelperText>
                         <div align="center">Guest controllers</div>
                     </FormHelperText>
-                    <RadioGroup row defaultValue="true">
+                    <RadioGroup row onChange={this.handleGuestCanPauseChange} defaultValue="true">
                         <FormControlLabel value="true" control={<Radio color="primary"/>} label="Play/Pause" labelPlacement="bottom"/>
                         <FormControlLabel value="false" control={<Radio color="secondary"/>} label="Disabled" labelPlacement="bottom"/>
                     </RadioGroup>
@@ -35,14 +93,14 @@ export default class RoomCreatePage extends Component{
             </Grid>
             <Grid item xs={12} align="center">
                 <FormControl>   
-                    <TextField required={true} type="number" defaultValue={this.default_votes} inputProps={{ min:1, style:{textAlign:"center"} }} />
+                    <TextField required={true} type="number" onChange={this.handleVotesChange} defaultValue={this.default_votes} inputProps={{ min:1, style:{textAlign:"center"} }} />
                     <FormHelperText>
                         <div align="center">Votes to skip</div>
                     </FormHelperText>
                 </FormControl>
             </Grid>
             <Grid item xs={12} align="center">
-                <Button color="primary" variant="contained">Create Room</Button> 
+                <Button color="primary" onClick={this.handleRoomCreatePressed} variant="contained">Create Room</Button> 
             </Grid>
             <Grid item xs={12} align="center">
                 <Button color="secondary" variant="contained" to="/react" component={Link}>Go Back</Button>
